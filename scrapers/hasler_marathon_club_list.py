1f618de10eeea61ca594df7204090919b89352a2
Will Abson <will.abson@gmail.com>
2015-04-27 16:04:40 +0100

    Switch club list processing to use CSV file from Google Drive rather than Excel sheet in Dropbox

--- scrapers/hasler_marathon_club_list.py
+++ scrapers/hasler_marathon_club_list.py
@@ -1,18 +1,23 @@
 import datetime
 import scraperwiki
-import xlrd
+#import xlrd
+import csv
+import urllib2
 
 keys = [ 'name', 'code', 'region_code' ]
 unique_keys = [ 'code' ]
 
 def main():
-    xlbin = scraperwiki.scrape("http://dl.dropbox.com/u/55716759/hrmTemplate.xlt")
-    book = xlrd.open_workbook(file_contents=xlbin)
-    sheet = book.sheet_by_name('Clubs')
-
-    for rownumber in range(0, sheet.nrows):
+    #xlbin = scraperwiki.scrape("http://dl.dropbox.com/u/55716759/hrmTemplate.xlt")
+    #book = xlrd.open_workbook(file_contents=xlbin)
+    #sheet = book.sheet_by_name('Clubs')
+    #for rownumber in range(0, sheet.nrows):
         # create dictionary of the row values
-        values = [ cellval(sheet.cell(rownumber, c), book.datemode) for c in range(0, 3) ]
+        #values = [ cellval(sheet.cell(rownumber, c), book.datemode) for c in range(0, 3) ]
+
+    f = urllib2.urlopen('https://googledrive.com/host/0B8A0SXNo_BkZb2hGUFphV1V3NWc')
+    reader = csv.reader(f)
+    for values in reader:
 
         # Only save if there are enough values
         if len(values) == len(keys):
